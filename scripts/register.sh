4f9d6d96e65fa08323f1a5e66a30b00aa40e417a
Byron Collins <Byron.Collins@datacom.co.nz>
2018-11-30 12:39:27 +1300

    removed scripts directory

--- scripts/register.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-subscription-manager remove --all
-subscription-manager unregister
-subscription-manager clean
-subscription-manager register --username $1 --password $2
-subscription-manager refresh
-subscription-manager attach --auto
--- scripts/unregister.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-subscription-manager remove --all
-subscription-manager unregister
-subscription-manager clean
